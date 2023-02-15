2b6846d39fe93d7efa238b0b27b657facd48ea52
Frances <frances@jobzi.com>
2023-02-15 07:57:07 -0500

    Ajusted files structure and docker-compose libraries

--- fastapi/logic/connectivityModel.py
+++
@@ -1,7 +0,0 @@
-class ConnectivityModel:
-    def __init__(self):
-        self.r = 2.0
-        self.s = 0.1
-    def predict(self, x: float) -> float:
-        y = self.r * x + self.s
-        return y
