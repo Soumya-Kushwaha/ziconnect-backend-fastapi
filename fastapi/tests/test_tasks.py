91904d50b2fbfa12df26fafad56d942ddc84867c
Frances <frances@jobzi.com>
2023-02-18 11:17:17 -0500

    Removed home method test

--- fastapi/tests/test_tasks.py
+++ fastapi/tests/test_tasks.py
@@ -4,10 +4,6 @@ from unittest.mock import patch, call
 
 from worker import create_task
 
-def test_home(test_app):
-    response = test_app.get("/")
-    assert response.status_code == 200
-
 def test_task():
     assert create_task.run(1)
     assert create_task.run(2)
