2c18c200b3e79e52e61183236014e983924546a3
Frances <frances@jobzi.com>
2023-02-18 20:26:53 -0500

    Update redis

--- fastapi/worker.py
+++ fastapi/worker.py
@@ -4,8 +4,8 @@ import time
 from celery import Celery
 
 celery = Celery(__name__)
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
+celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
 
 
 @celery.task(name="create_task")
