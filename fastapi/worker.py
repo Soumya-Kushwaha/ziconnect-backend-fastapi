874133e0111e97100689bd87de8b115baecc6aff
Frances <frances@jobzi.com>
2023-02-18 19:15:38 -0500

    Update bind ip redis yml file

--- fastapi/worker.py
+++ fastapi/worker.py
@@ -4,8 +4,8 @@ import time
 from celery import Celery
 
 celery = Celery(__name__)
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379")
-celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379")
+celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://0.0.0.0:6379")
+celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://0.0.0.0:6379")
 
 
 @celery.task(name="create_task")
