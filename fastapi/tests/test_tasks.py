1ca6bfe34af47ba83bdc5761c9f93c3c49456ed1
Frances <frances@jobzi.com>
2023-02-18 21:06:37 -0500

    Added file docs in the project structure

--- fastapi/tests/test_tasks.py
+++ fastapi/tests/test_tasks.py
@@ -20,4 +20,23 @@ def test_mock_task(mock_run):
     assert create_task.run(3)
     assert create_task.run.call_count == 3
 
+"""
+def test_task_status(test_app):
+    response = test_app.post(
+        "/task/prediction",
+        data=json.dumps({"predictionType": 1})
+    )
+    content = response.json()
+    task_id = content["task_id"]
+    assert task_id
 
+    response = test_app.get(f"task/result/{task_id}")
+    content = response.json()
+    assert content == {"task_id": task_id, "task_status": "PENDING", "task_result": None}
+    assert response.status_code == 200
+
+    while content["task_status"] == "PENDING":
+        response = test_app.get(f"task/result/{task_id}")
+        content = response.json()
+    assert content == {"task_id": task_id, "task_status": "SUCCESS", "task_result": True}   
+"""
