3857ae949119c564eb67a4e5e3463c85dee4a5f9
Frances <frances@jobzi.com>
2023-02-28 10:01:01 -0500

    Adjusted import and json process for the task function

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,11 +1,15 @@
-from typing import Union
+import os
+import json
 from fastapi import FastAPI, Body, Form, Request, File, UploadFile, Path
+import worker 
+from datetime import datetime
+from time import mktime
+from typing import Union
 from celery.result import AsyncResult
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
-from worker import create_task
 
 tags_metadata = [
     {
@@ -32,16 +36,51 @@ templates = Jinja2Templates(directory="html")
 def run_task(predictionType: int = Form(...),
              predictionFile: UploadFile = File(...)):
     task_type = predictionType
-    task = create_task(task_type,predictionFile)
-    return JSONResponse({"task_id": task.id})
+    task_name = "uploadFile_task"
 
+    """ CheckFile """
+    fileName = predictionFile.filename 
+    targetDir = '/usr/src/app/files/'
+    filePath = os.path.join(targetDir, fileName)
+    with open(filePath, mode='wb+') as f:
+        f.write(predictionFile.file.read())
+
+    result = worker.app.send_task(task_name, args=[filePath], kwargs={},queue='celery', routing_key='key_result_processing')
+    return JSONResponse({"task_id": result.id})
 
 @app.get("/task/result/{task_id}", tags=["result"])
 def get_status(task_id):
-    task_result = AsyncResult(task_id)
+    task_result =  worker.app.AsyncResult(task_id)
     result = {
         "task_id": task_id,
         "task_status": task_result.status,
         "task_result": task_result.result
     }
     return JSONResponse(result)
+
+
+class EncoderObj(json.JSONEncoder):   
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return {
+                '__type__': '__datetime__', 
+                'epoch': int(mktime(obj.timetuple()))
+            }
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+def decoderObj(obj):
+    if '__type__' in obj:
+        if obj['__type__'] == '__datetime__':
+            return datetime.fromtimestamp(obj['epoch'])
+    return obj
+
+# Encoder function      
+def dumpEncode(obj):
+    return json.dumps(obj, cls=EncoderObj)
+
+# Decoder function
+def loadDecode(obj):
+    return json.loads(obj, object_hook=decoderObj)
+
+
