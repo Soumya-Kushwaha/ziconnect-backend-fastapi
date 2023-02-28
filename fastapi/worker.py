850859d1f3a60307111ccbafa1f47f76cd95bf72
Frances <frances@jobzi.com>
2023-02-28 10:00:21 -0500

    Fixed import and json process by taskID

--- fastapi/worker.py
+++ fastapi/worker.py
@@ -1,38 +1,34 @@
 import os
-import base64
 from time import sleep
 from celery import Celery
-from fastapi import  FastAPI, File, UploadFile
+from celery.utils.log import get_task_logger
 import csv
+import traceback
 
-celery = Celery(__name__)
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+app = Celery(__name__,include=['worker', 'celery.app.builtins'])
+app.conf.broker_url = os.getenv("CELERY_BROKER_URL", "amqp://localhost:5672")
+app.conf.result_backend = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+app.conf.update(result_extended=True)
+celery_log = get_task_logger(__name__)
+
+@app.task(name="uploadFile_task", bind=True)
+def uploadFile_task(filePath):
+    try:        
+        """ Convert to JsonFormat """
+        data = {}
+        with open(filePath, mode='r', encoding='utf-8') as csvf:
+            csvReader = csv.DictReader(csvf)
+            for rows in csvReader:             
+                key = rows['school_name']
+                data[key] = rows    
+        return {"result": "Escolas {}".format(data)}
+    except Exception as ex:
+            meta={
+                'exc_type': type(ex).__name__,
+                'exc_message': traceback.format_exc().split('\n')
+                 }
+    raise meta
 
 
-@celery.task(name="create_task")
-def create_task(predictionType,predictionFile):
-    idTaskCelery = celery.AsyncResult.task_id
-    uploadFile_task(idTaskCelery,predictionFile)   
-    return True
 
-
-#@celery.task(name="uploadfile_task")
-async def uploadFile_task(idTaskCelery, predictionFile):
-    
-    """ CheckFile """
-    fileName = predictionFile.filename
-    targetDir = '/usr/src/app/files/'
-    filePath = os.path.join(targetDir, fileName)
-    with open(filePath, mode='wb+') as f:
-        f.write(predictionFile.file.read())
-    
-    """ Convert to JsonFormat """
-    data = {}
-    with open(filePath, mode='r', encoding='utf-8') as csvf:
-        csvReader = csv.DictReader(csvf)
-        for rows in csvReader:             
-            key = rows['school_name']
-            data[key] = rows    
-    #return {data}
 
