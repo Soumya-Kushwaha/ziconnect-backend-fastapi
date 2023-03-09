dfb59debe2f10eb9adc462a1ce4a22bc1b2a419e
Frances <frances@jobzi.com>
2023-03-09 06:05:19 -0500

    Added test checkHealth api

--- fastapi/tests/test_tasks.py
+++ fastapi/tests/test_tasks.py
@@ -1,45 +1,17 @@
 import json
+import os
 from unittest.mock import patch, call
 from worker import uploadFile_task
+from main import *
 
 
-def test_task():
-    """
-    assert create_task.run(1,None)
-    assert create_task.run(2,None)
-    assert create_task.run(3,None)
-    """
+def test_service_health_ok():
+    from main import service_health 
+    with service_health.raises(JSONResponse(content='ok', status_code=200)):
+        service_health()
 
-@patch("worker.uploadFile_task.run")
-def test_mock_task(mock_run):
-    """
-    assert create_task.run(1,None)
-    create_task.run.assert_called_once_with(1)
+def test_service_health_nok():
+    from main import service_health 
+    with service_health.raises(Exception):
+        service_health()
 
-    assert create_task.run(2,None)
-    assert create_task.run.call_count == 2
-
-    assert create_task.run(3,None)
-    assert create_task.run.call_count == 3
-    """
-
-"""
-def test_task_status(test_app):
-    response = test_app.post(
-        "/task/prediction",
-        data=json.dumps({"predictionType": 1})
-    )
-    content = response.json()
-    task_id = content["task_id"]
-    assert task_id
-
-    response = test_app.get(f"task/result/{task_id}")
-    content = response.json()
-    assert content == {"task_id": task_id, "task_status": "PENDING", "task_result": None}
-    assert response.status_code == 200
-
-    while content["task_status"] == "PENDING":
-        response = test_app.get(f"task/result/{task_id}")
-        content = response.json()
-    assert content == {"task_id": task_id, "task_status": "SUCCESS", "task_result": True}   
-"""
