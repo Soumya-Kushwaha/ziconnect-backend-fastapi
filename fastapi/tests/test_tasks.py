1dbe6b121bb1079d1d877bfce5e385e52e12270b
Frances <frances@jobzi.com>
2023-03-14 10:37:37 -0400

    Merge from stagging

--- fastapi/tests/test_tasks.py
+++ fastapi/tests/test_tasks.py
@@ -5,6 +5,8 @@ from unittest import TestCase
 from worker import uploadFile_task
 from main import app
 from starlette.testclient import TestClient
+import json
+import pandas as pd
 
 client = TestClient(app)
 
@@ -16,21 +18,32 @@ def test_health():
 
 def test_taskPrediction():
 
-    row_list = [["id", "country_code", "country_name"],
-                [1, "BR", "Brasil"]
-                [2, "BR", "Brazil"]]
-    with open('locality.csv', 'w', newline='') as locality:
-        writer = csv.writer(locality)
-        writer.writerows(row_list)
+    df = pd.DataFrame({
+               'state_code': ['CA', 'CA', 'NY', 'NY', 'NY'],
+               'municipality_code': ['LA', 'LA', 'NYC', 'NYC', 'BUF'],
+               'school_code': ['1', '2', '3', '4', '5'],
+               'school_type': ['State', 'Local', 'Federal', 'State', 'State'],
+               'school_region': ['Urban', 'Rural', 'Urban', 'Urban', 'Urban'],
+               'student_count': [100, 200, 150, 300, 250],
+               'internet_availability': ['Yes', 'No', 'Yes', 'NA', 'NA'],
+               'internet_availability_prediction': ['Yes', 'No', 'Yes', 'No', 'No'],
+           })
+    
 
-    form_data = {
-        "locality_file": locality,
-        "school_file": locality
+    dataModeling = {
+        "locality_file": df,
+        "school_file": df
     }
 
-    response = client.post("/task/prediction", 
-                            json=form_data,
-                            headers={ 'Content-Type': 'application/x-www-form-urlencoded'})
 
-    result = response.json()
-    assert result.status_code == 200
+    response = client.post("/task/prediction",  
+                            params=dataModeling,
+                            headers={ 'Content-Type': 'application/x-www-form-urlencoded'})
+    
+    assert response.status_code != 200
+
+
+def test_taskResult():
+    task_id = '5984d769-7805-4fdb-81fc-da68fad134fe'
+    response = client.get("/task/result/{task_id}")
+    assert response.status_code != 200
