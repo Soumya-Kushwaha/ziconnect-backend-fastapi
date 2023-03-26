8e6f4fb9b84f1443687215a2150aa1d8943b9da5
Frances <frances@jobzi.com>
2023-03-26 00:36:00 -0400

    Update methods for test

--- fastapi/tests/test_worker.py
+++ fastapi/tests/test_worker.py
@@ -1,6 +1,8 @@
 from unittest.mock import patch
 from worker import uploadFile_task,uploadSocialImpactFile_task
 
+import unittest
+
 @patch('worker.uploadFile_task')
 def test_uploadFile_task(return_uploadFile_mock):
     try:
@@ -14,5 +16,17 @@ def test_uploadSocialImpactFile_task(return_uploadSocialImpactFile_mock):
     try:
         uploadSocialImpactFile_task.apply()
         assert return_uploadSocialImpactFile_mock.called
+    except AssertionError as msg:
+        print(msg)
+
+
+class TestAddTask(unittest.TestCase):
+  def setUp(self):
+    try:
+        localityFile = '/var/lib/docker/volumes/fastapi-storage/_data/locality.csv'
+        schoolFile = '/var/lib/docker/volumes/fastapi-storage/_data/school.csv'
+
+        self.task = uploadFile_task.apply_async(localityFile,schoolFile)
+        assert self.results == self.task.get()
     except AssertionError as msg:
         print(msg)
